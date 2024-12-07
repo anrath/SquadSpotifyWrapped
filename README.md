3e7b373008992dc0f88230e7dc48d53208b513f8
anrath <aklounts@gmail.com>
2024-12-07 14:07:02 -0500

    init

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# squad-spotify-wrapped
+## SquadSpotifyWrapped - Spotify Wrapped For Your Squad (By Kasra)
+
+I wanted a better way to experience my squad&apos;s music so I built it. Open source because why not. Free because it only runs on client.
+
+## Credits
+
+[Theo Browne](https://github.com/t3dotgg) and collaborators for the open-source [QuickPic](https://quickpic.t3.gg) project which was the foundation for this codebase.
---
+++ src/app/page.tsx
@@ -0,0 +1,41 @@
+import { RoundedTool } from "./rounded-tool";
+
+export const metadata = {
+  title: "Corner Rounder - SquadSpotifyWrapped",
+  description: "Round corners on an image (for free because duh)",
+};
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col justify-between p-8 font-[family-name:var(--font-geist-sans)] sm:p-20">
+      <main className="flex flex-grow flex-col items-center justify-center">
+        <div>
+          Hi. I&apos;m{" "}
+          <a
+            href="https://kasralekan.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline"
+          >
+            Kasra
+          </a>
+          . I built this because I wanted my squad to experience each
+          other&apos;s music. I hope you enjoy it.
+        </div>
+        <div className="mt-4">
+          <RoundedTool />
+        </div>
+      </main>
+      <footer className="mt-8 text-center text-sm text-gray-500">
+        <a
+          href="https://github.com/t3dotgg/SquadSpotifyWrapped"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:underline"
+        >
+          View on GitHub
+        </a>
+      </footer>
+    </div>
+  );
+}
